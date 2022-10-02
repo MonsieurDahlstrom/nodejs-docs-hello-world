d4e48b547abd0107eb9cebfc37d167f61bdd8257
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2022-10-02 13:12:54 +0200

    debug secret

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const secret =
   process.env.gotSecret === true ? "Has Secret" : "No Configured Secret";
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(`Hello, ${secret}`);
+  response.end(`Hello, ${secret} '${process.env.gotSecret}'`);
 });
 
 const port = process.env.PORT || 1337;
